eaa5b376eb2da88d0144c83a5eddc9149caecbf4
Isaac Griffith <isaacgriffith@gmail.com>
2017-12-25 06:12:14 -0700

    Added base files for the assignment

---
+++ [lastname].[firstname].ruby_lab.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/ruby
+
+###############################################################
+#
+# CSCI 305 - Ruby Programming Lab
+#
+# <firstname> <lastname>
+# <email-address>
+#
+###############################################################
+
+$name = "<firstname> <lastname>"
+
+# function to process each line of a file and extract the song titles
+def process_file(file_name)
+	puts "Processing File.... "
+	
+	begin
+		IO.foreach(file_name) do |line|
+			# do something for each line
+		end
+
+		puts "Finished. Bigram model built.\n"
+	rescue
+		STDERR.puts "Could not open file"
+		exit 4
+	end
+end
+
+# Executes the program
+def main_loop()
+	puts "CSCI 305 Ruby Lab submitted by #{$name}"	
+
+	if ARGV.length < 1
+		puts "You must specify the file name as the argument."
+		exit 4
+	end
+	
+	# process the file
+	process_file(ARGV[0])
+
+	# Get user input
+end
+
+main_loop()
