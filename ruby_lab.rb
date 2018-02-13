a2fc426347008e282b2506aa8db04e27809d7042
wizmariefa <mariecolee1@gmail.com>
2018-02-12 22:23:18 -0700

    Final commit

--- ruby_lab.rb
+++ ruby_lab.rb
@@ -34,17 +34,22 @@ def process_file(file_name)
 					end
 			end
 		end
-		#search = $stdin.gets.chomp
-		#if search
-			#mcw(search)
-		#end
-		mcw("love")
+
+		search = ""
+		while search != "q"
+			puts("Enter a word [Enter 'q' to quit]:")
+			search = $stdin.gets.chomp
+			if (search != "q")
+				print(create_title(search))
+			end
+			puts ""
+		end
+	end
 		puts "Finished. Bigram model built.\n"
 	#rescue
 		#STDERR.puts "Could not open file"
-		#exit 4
+		#exit
 	end
-end
 
 def cleanup_title(songTitle)
 	title = songTitle.gsub(/(.)+<SEP>/, "") # strips everything but title
@@ -59,7 +64,13 @@ end
 
 def addtoB(title)
 	#title = "let's see what this is doing"
+	stops = [" a ", " an ", " and ", " by ", " for ", " from ", " in ", " of ", " on ", " or ", " out ", " the ", " to ", " width "]
+	for word in stops do
+			title = title.gsub(word, "") # changes word to NOTHING!
+		end
+
 	title_words = title.split(" ") # splits title into various words
+
 	while (title_words.length > 1)
 		first_word = title_words[0] # saves the first word to title bigram
 		next_wrd = title_words[1]
@@ -75,20 +86,42 @@ def addtoB(title)
 				else
 				$bigrams[first_word].merge!(next_wrd => 1)
 			end
+		end
 	end
-end
 
 def mcw(search)
 		if !$bigrams.has_key?(search)
-			puts "Error. Key does not exist"
+			most_common = nil
 
-		else return $bigrams[search].max_by{|word, number| number}[0] # search for max by # of maxes
+		else most_common = $bigrams[search].max_by{|word, number| number}[0] # search for max by # of maxes
 		end
+
+		return most_common
 end
 
 def create_title(word)
-
+	current = word
+	word_num = 1
+	title = ""
+	title += word
+	while word_num !=20 # while we have less than 20 words...
+			if ($bigrams.has_key?(current)) # if the word exists in the bigram
+				if (mcw(current) == nil)
+					# do nothing and exit
+					word_num = 20
+				else
+					addition = mcw(current) # thing to add is mcw
+					title += " "
+					title += addition
+					current = addition # set current to the new wordtitle += addition # add the mcw
+					word_num += 1 # increment by one and then go throuh
+				end
+			else word_num = 20
+			end
+		end
+		return title
 end
+
 # Executes the program
 def main_loop()
 	puts "CSCI 305 Ruby Lab submitted by #{$name}"
